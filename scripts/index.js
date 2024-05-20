4aa9de76cc55261a2c780bdf853e13cca110fb08
a.leshko <a.leshko@bsuir.by>
2024-05-20 13:36:28 +0300

    init: initial commit ( Mon 20.05.2024 13:36 )

---
+++ scripts/index.js
@@ -0,0 +1,54 @@
+// Header scroll animation
+const logoText = document.getElementById('header__logoText');
+const burgerMenuText = document.getElementById('item--burgerMenuText');
+
+window.addEventListener('scroll', () => {
+    if (document.documentElement.scrollTop > 40) {
+        burgerMenuText.style.display = 'none';
+    } else {
+        burgerMenuText.style.display = '';
+    }
+})
+
+// Burger menu open/close buttons
+const burgerMenu = document.getElementById('burgerMenu');
+const openBurgerButton = document.getElementById('item--burgerMenu');
+const closeBurgerButton = document.getElementById('burgerMenu__closeButton');
+
+openBurgerButton.addEventListener('click', () => {
+    burgerMenu.style.left = '0';
+})
+
+closeBurgerButton.addEventListener('click', () => {
+    burgerMenu.style.left = '-101vw';
+})
+
+// Menu horizontal scroll by default
+let elems = document.getElementsByClassName('list--levelTwo');
+
+for (let elem of elems) {
+    elem.addEventListener('wheel', (e) => {
+        e.preventDefault();
+        elem.scrollLeft += e.deltaY;
+    })
+}
+
+// Burger menu list script
+const levelOne_items = document.getElementsByClassName('list--levelOne')[0].children;
+const levelTwo_items = document.getElementsByClassName('list--levelTwo');
+
+for (let i = 0; i < levelOne_items.length; i++) {
+    levelOne_items[i].addEventListener('click', () => {
+        for (let levelOne_item of levelOne_items) {
+            levelOne_item.classList.remove('item--selected')
+        }
+
+        for (let levelTwo_item of levelTwo_items) {
+            levelTwo_item.classList.add('d-none');
+        }
+
+        levelOne_items[i].classList.add('item--selected');
+        levelTwo_items[i].classList.remove('d-none');
+    })
+    console.log(levelOne_items[i])
+}
