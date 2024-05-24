27b401789bdac1d5212b74ba55d8a33a20cadc58
a.leshko <a.leshko@bsuir.by>
2024-05-24 15:37:36 +0300

    style: переработана html-структура и css отдельных элементов ( Fri 24.05.2024 15:37 )

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 // Burger menu open/close buttons
-const burgerMenu = document.getElementById('burgerMenu');
-const openBurgerButton = document.getElementById('item--burgerMenu');
-const closeBurgerButton = document.getElementById('burgerMenu__closeButton');
+const burgerMenu = document.getElementById('burger__wrapper');
+const openBurgerButton = document.getElementById('item--burgerButton');
+const closeBurgerButton = document.getElementById('burger__closeButton');
 
 openBurgerButton.addEventListener('click', () => {
     burgerMenu.style.left = '0';
@@ -12,8 +12,8 @@ closeBurgerButton.addEventListener('click', () => {
 })
 
 // Burger menu list script
-const levelOne_items = document.getElementsByClassName('itemsContainer__item');
-const levelTwo_items = document.getElementsByClassName('list--levelTwo');
+const levelOne_items = document.getElementsByClassName('item--levelOne');
+const levelTwo_lists = document.getElementsByClassName('list--levelTwo');
 
 for (let i = 0; i < levelOne_items.length; i++) {
     levelOne_items[i].addEventListener('click', () => {
@@ -21,34 +21,21 @@ for (let i = 0; i < levelOne_items.length; i++) {
             levelOne_item.classList.remove('item--selected')
         }
 
-        for (let levelTwo_item of levelTwo_items) {
-            levelTwo_item.classList.add('d-none');
+        for (let levelTwo_list of levelTwo_lists) {
+            levelTwo_list.classList.add('d-none');
         }
 
         levelOne_items[i].classList.add('item--selected');
-        levelTwo_items[i].classList.remove('d-none');
+        levelTwo_lists[i].classList.remove('d-none');
     })
-    console.log(levelOne_items[i])
 }
 
 // Burger menu horizontal scroll by default
-let elems = document.getElementsByClassName('list--levelTwo');
+let lists = document.getElementsByClassName('list--levelTwo');
 
-for (let elem of elems) {
-    elem.addEventListener('wheel', (e) => {
+for (let list of lists) {
+    list.addEventListener('wheel', (e) => {
         e.preventDefault();
-        elem.scrollLeft += e.deltaY;
+        list.scrollLeft += e.deltaY;
     })
 }
-
-// Header scroll animation
-const logoText = document.getElementById('header__logoText');
-const burgerMenuText = document.getElementById('item--burgerMenuText');
-
-window.addEventListener('scroll', () => {
-    if (document.documentElement.scrollTop > 40) {
-        burgerMenuText.style.display = 'none';
-    } else {
-        burgerMenuText.style.display = '';
-    }
-})
