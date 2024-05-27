68acd6906a87600556b20a57777bd0ce1f015905
Alexandr Leshko <sasha.leshko99@gmail.com>
2024-05-27 23:12:21 +0300

    style: Прописаны стили для 768px — 360px ( Sun 27.05.2024 23:12 )

---
+++ scripts/burger.js
@@ -0,0 +1,43 @@
+// Burger open/close buttons
+const burgerWrapper = document.getElementById('burgerWrapper');
+const burgerOpen = document.getElementById('burgerOpen');
+const burgerClose = document.getElementById('burgerClose');
+burgerOpen.addEventListener('click', () => {
+    burgerWrapper.style.left = '0';
+})
+burgerClose.addEventListener('click', () => {
+    burgerWrapper.style.left = '-101vw';
+})
+
+// Burger menu PC
+const burgerMenuPC = document.getElementById('burgerMenuPC');
+const levelOneitemsPC = burgerMenuPC.getElementsByClassName('item--levelOne');
+const levelTwolistsPC = burgerMenuPC.getElementsByClassName('list--levelTwo');
+for (let i = 0; i < levelOneitemsPC.length; i++) {
+    levelOneitemsPC[i].addEventListener('click', () => {
+        for (let levelOneitemPC of levelOneitemsPC) {
+            levelOneitemPC.classList.remove('item--selected');
+        }
+
+        for (let levelTwolistPC of levelTwolistsPC) {
+            levelTwolistPC.classList.add('d-none');
+        }
+
+        levelOneitemsPC[i].classList.add('item--selected');
+        levelTwolistsPC[i].classList.remove('d-none');
+    })
+}
+
+// Burger menu Mobile
+const burgerMenuMobile = document.getElementById('burgerMenuMobile');
+const levelOne_items = burgerMenuPC.getElementsByClassName('item--levelOne');
+const levelTwo_lists = burgerMenuPC.getElementsByClassName('list--levelTwo');
+
+// Burger horizontal scroll by default
+let lists = document.getElementsByClassName('list--levelTwo');
+for (let list of lists) {
+    list.addEventListener('wheel', (e) => {
+        e.preventDefault();
+        list.scrollLeft += e.deltaY;
+    })
+}
